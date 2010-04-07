6a2fd1c56f1e99c4554a5a5d7383ac6ca291f19b
Kwang Gi Kim <kim3492@commax.co.kr>
2010-04-07 12:52:33 +0900

    mistake merge new branch and master

---
+++ newBranchHelloWorld.java
@@ -0,0 +1,15 @@
+public class Hellogit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sysetm.out.println(" current branch : new branch ");
+		Sysetm.out.println(" changes msater to new branch ");
+		Sysetm.out.println(" 원격 저장소에 추가한후... Sys... 추가");
+		Sysetm.out.println(" 원격 저장소에 추가한후... Sys... 추가.. 그러나 merge를 누락해서.. 다시 merge를 시행 함");
+
+	}
+
+}
